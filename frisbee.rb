f286191bf46bc95f61d9b1bc6ece43ed6f62ad72
Kyle Tolle <kyle@nullsix.com>
2012-05-05 12:45:34 -0600

    Refactored a lot. Created a physics class for constants. Renamed the Action class to be Flight. Changed the name of the functions that store the data.

--- frisbee.rb
+++ frisbee.rb
@@ -1,61 +1,78 @@
-class Action
-  attr_accessor :x0, :y0, :vx0, :vy0, :g, :rho, :vx, :vy, :x, :y, :m, :area, :clo, :cla, :cdo, :cda, :alpha0, :alpha, :cl, :cd, :dt
+class Physics
+    G = -9.81 # Gravity in m/s
+    RHO = 1.23 # Air density in kg/m^3
+    AREA = 0.0568 # Area of average frisbee
+    CLO = 0.1 # Lift Coefficient at Alpha=0
+    CLA = 1.4 # Lift Coefficient at Alpha
+    CDO = 0.08 # Drag Coeff at Alpha=0
+    CDA = 2.72 # Drag Coeff at Alpha
+    ALPHA0 = -4.0 # Characterizes angular dependence of drag due to frisbee shape
+    M = 0.175 # Average frisbee mass in kg
+end
+
+class Flight
+  attr_accessor :vx, :vy, :x, :y, :alpha, :cl, :cd, :dt
 
   def initialize
     begin
+      # Read in the frisbee's initial conditions
       f = File.open("param.dat")
+      # This reads in the single line, breaks it on spaces and converts the
+      # values to floats before assigning them all to variables
       line = f.readline
-      self.alpha, self.x0, self.vx0, self.y0, self.vy0, self.dt = line.split.map(&:to_f)
+      self.alpha, x0, vx0, y0, vy0, self.dt = line.split.map(&:to_f)
 
+      #Open the output file
       @out = File.open("data.rb.dat","w")
+
     rescue => e
       puts e
       exit
     end
-
-    self.g = -9.81
-    self.rho = 1.23
-    self.area = 0.0568
-    self.clo = 0.1
-    self.cla = 1.4
-    self.cdo = 0.08
-    self.cda = 2.72
-    self.alpha0 = -4.0
-
-    self.m = 0.175
 
     self.vx = vx0
     self.x = x0
     self.y = y0
     self.vy = vy0
 
-    self.cl = clo + cla*alpha*(Math::PI/180)
+    self.cl = Physics::CLO + Physics::CLA*alpha*(Math::PI/180)
 
-    self.cd = cdo + cda*(((alpha-alpha0)*(Math::PI/180))**2)
+    self.cd = Physics::CDO + Physics::CDA*(((alpha-Physics::ALPHA0)*(Math::PI/180))**2)
 
     @data = ""
   end
 
-  #TODO: RENAME THIS!
-  def print
-    values = "#{x} #{y} #{vx} #{vy}"
-    @data << values + "\n"
-  end
-
   def run
-    print
+    output
+    # Perform the simulation while the frisbee is still in the air
     while y > 0
       iterate
-      print
+      output
     end
+
+    # Write the data to the file
     @out << @data
   end
 
   private
-    def iterate
-      deltavx = -(rho*(vx**2)*area*cd*dt)
-      deltavy = ((rho*(vx**2)*area*cl)/(2*m)+g)*dt
+    # Appends new state data to existing data
+    def output
+      @data << state + "\n"
+    end
 
+    # Serialized state data
+    def state
+      "#{x} #{y} #{vx} #{vy}"
+    end
+
+    # Perform the next step of the frisbee simulation
+    def iterate
+      # Change in horizontal velocity
+      deltavx = -(Physics::RHO*(vx**2)*Physics::AREA*cd*dt)
+      # Change in vertical velocity
+      deltavy = ((Physics::RHO*(vx**2)*Physics::AREA*cl)/(2*Physics::M)+Physics::G)*dt
+
+      # Update the frisbee's velocity and position
       self.vx = vx + deltavx
       self.vy = vy + deltavy
       self.x = x + vx*dt
@@ -63,6 +80,7 @@ class Action
     end
 end
 
-a = Action.new
 
-a.run
+fribee_sim = Flight.new
+
+fribee_sim.run
